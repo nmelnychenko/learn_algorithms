7d2cf0ec2f4fa581409333f83dda1a668260d6c5
nmelnychenko <mykolam@seekingalpha.com>
2022-08-12 18:46:17 +0300

    put sorting function calls inside test body

--- bubbleSorting/main.cpp
+++ bubbleSorting/main.cpp
@@ -27,54 +27,92 @@ int * BubbleSwap(int arr[], int arrSize) {
   return arr;
 }
 
-int * randomLongArray() {
-  static int randomArr[10000];
-  srand(time(NULL));
-
-  for (int i = 0; i < 10000; i++) {
-    int no = rand()%1000;
-    randomArr[i] = no;
-  }
-
-  return randomArr;
-}
-
-int testArr1[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-int testArr2[5] = {5, 4, 1, 2, 3};
-int testArr3[1] = {7};
-int testArr4[22] = {1, 1, 1, 1, 2, 3, 4, 5, 1, 1, 1, 2, 2,
-  2, 3, 3, 4, 5, 3, 3, 3, 2};
-int *testArr5 = randomLongArray();
-int testArr6[10] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-int testArr7[0] = {};
-
-int *sortedArr1 = BubbleSwap(testArr1, 10);
-int *sortedArr2 = BubbleSwap(testArr2, 5);
-int *sortedArr3 = BubbleSwap(testArr3, 1);
-int *sortedArr4 = BubbleSwap(testArr4, 22);
-int *sortedArr5 = BubbleSwap(testArr5, 10000);
-int *sortedArr6 = BubbleSwap(testArr6, 10);
-int *sortedArr7 = BubbleSwap(testArr7, 0);
-
 struct BubbleSorting : public Test {};
 
 // Short Sorted Array
-TEST_F(BubbleSorting, ShortSortedArray) { EXPECT_TRUE(std::is_sorted(sortedArr1, sortedArr1 + 10)); }
+TEST_F(BubbleSorting, ShortSortedArray) {
+  int source[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  int etalon[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  int *sorted = BubbleSwap(source, 10);
+
+  for (int i = 0; i < 10; ++i) {
+    EXPECT_EQ(sorted[i], etalon[i]) << "Arrays sorted and etalon differ at index " << i;
+  }
+}
 
 // Short Unsorted Array
-TEST_F(BubbleSorting, ShortUnsortedArray) { EXPECT_TRUE(std::is_sorted(sortedArr2, sortedArr2 + 5)); }
+TEST_F(BubbleSorting, ShortUnsortedArray) {
+  int source[] = {5, 4, 1, 2, 3};
+  int etalon[] = {1, 2, 3, 4, 5};
+  int *sorted = BubbleSwap(source, 5);
+
+  for (int i = 0; i < 5; ++i) {
+    EXPECT_EQ(sorted[i], etalon[i]) << "Arrays sorted and etalon differ at index " << i;
+  }
+}
 
 // Single element array
-TEST_F(BubbleSorting, SignleElementArray) { EXPECT_TRUE(std::is_sorted(sortedArr3, sortedArr3 + 1)); }
+TEST_F(BubbleSorting, SignleElementArray) {
+  int source[] = {5};
+  int etalon[] = {5};
+  int *sorted = BubbleSwap(source, 1);
+
+  for (int i = 0; i < 1; ++i) {
+    EXPECT_EQ(sorted[i], etalon[i]) << "Arrays sorted and etalon differ at index " << i;
+  }
+}
 
 // Array with duplicates
-TEST_F(BubbleSorting, ArrayWithDuplicates) { EXPECT_TRUE(std::is_sorted(sortedArr4, sortedArr4 + 22)); }
+TEST_F(BubbleSorting, ArrayWithDuplicates) {
+  int source[22] = {1, 1, 1, 1, 2, 3, 4, 5, 1, 1, 1, 2, 2,
+   2, 3, 3, 4, 5, 3, 3, 3, 2};
+  int etalon[22] = {1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3,
+   3, 3, 3, 3, 3, 4, 4, 5, 5};
+  int *sorted = BubbleSwap(source, 22);
+
+  for (int i = 0; i < 22; ++i) {
+    EXPECT_EQ(sorted[i], etalon[i]) << "Arrays sorted and etalon differ at index " << i;
+  }
+}
 
 // Very long array with random values
-TEST_F(BubbleSorting, LongRandomArray) { EXPECT_TRUE(std::is_sorted(sortedArr5, sortedArr5 + 10000)); }
+TEST_F(BubbleSorting, LongRandomArray) {
+  int source[10000];
+  for (int i = 0; i < 10000; i++) {
+    source[i] = rand()%100;
+  }
+
+  int etalon[10000];
+
+  std::copy(std::begin(source), std::end(source), std::begin(etalon));
+  std::sort(etalon, etalon + 10000);
+
+  int *algo_sorted = BubbleSwap(source, 10000);
+
+  for (int k = 0; k < 10000; k++) {
+    EXPECT_EQ(algo_sorted[k], etalon[k]) << "Arrays sorted and etalon differ at index " << k;
+  }
+}
 
 // Reverse sorted array
-TEST_F(BubbleSorting, ReversedArray) { EXPECT_TRUE(std::is_sorted(sortedArr6, sortedArr6 + 10)); }
+TEST_F(BubbleSorting, ReversedArray) {
+  int source[10] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+  int etalon[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  int *sorted = BubbleSwap(source, 10);
+
+  for (int i = 0; i < 10; ++i) {
+    EXPECT_EQ(sorted[i], etalon[i]) << "Arrays sorted and etalon differ at index " << i;
+  }
+}
 
 // Empty array
-TEST_F(BubbleSorting, EmptyArray) { EXPECT_TRUE(std::is_sorted(sortedArr7, sortedArr7)); }
+TEST_F(BubbleSorting, EmptyArray) {
+  int source[] = {};
+  int etalon[] = {};
+  int *sorted = BubbleSwap(source, 0);
+  int size = *(&sorted + 1) - sorted;
+
+  for (int i = 0; i < size; ++i) {
+    EXPECT_EQ(sorted[i], etalon[i]) << "Arrays sorted and etalon differ at index " << i;
+  }
+}
