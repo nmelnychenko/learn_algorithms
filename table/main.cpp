5df3870f6c28614561e5a5493d6ab9606c9ee538
nmelnychenko <mykolam@seekingalpha.com>
2022-08-29 23:51:13 +0300

    finish example of table usage

--- table/main.cpp
+++ table/main.cpp
@@ -4,9 +4,56 @@
 
 using namespace testing;
 
-struct Stack : public Test {};
+struct TableTest : public Test {};
 
-// Test size
-TEST_F(Table, size) {
+// Test put 1 element
+TEST_F(TableTest, putSingle) {
+  Table<string> testTable;
+  string testValue = "testValue";
+  string testKey = "testKey";
 
+  testTable.put(testKey, testValue);
+
+  // Test that table has only 1 elements
+  EXPECT_TRUE(testTable.Count() == 1);
+
+  // Test item value by key
+  EXPECT_EQ(testTable.valueAt(testKey), testValue);
+}
+
+// Test put element to full Table
+TEST_F(TableTest, putFull) {
+  Table<string> testTable1;
+  string testValue = "testValue";
+  string testKey = "testKey";
+
+  for (int i = 0; i < 1000; i++) {
+    testTable1.put(to_string(i), to_string(rand()));
+  }
+
+  // Test that table has exactly 1000 elements (full)
+  EXPECT_TRUE(testTable1.Count() == 1000);
+
+  // Should not put to full table
+  EXPECT_THROW(testTable1.put(testKey, testValue), std::length_error);
+}
+
+// Test erase element
+TEST_F(TableTest, eraseSingle) {
+  Table<string> testTable2;
+  string testValue = "testValue";
+  string testKey = "testKey";
+
+  testTable2.put(testKey, testValue);
+
+  // Test that table has only 1 elements
+  EXPECT_TRUE(testTable2.Count() == 1);
+
+  testTable2.erase(testKey);
+
+  // Test that table has not any elements
+  EXPECT_TRUE(testTable2.Count() == 0);
+
+  // Test that table has not values by testKey
+  EXPECT_THROW(testTable2.valueAt(testKey), std::invalid_argument);
 }
--- table/table.h
+++ table/table.h
@@ -1,5 +1,5 @@
-#include <iostream>
 #include <string>
+#include <iostream>
 #include <exception>
 #include <stdexcept>
 
@@ -7,16 +7,15 @@ using namespace std;
 
 const int CAPACITY = 1000;
 
-// template <typename KeyT, typename ValueT>
-struct Item
-{
+template <typename ValueT> struct Item {
 	string key;
-  string value;
+  ValueT value;
 };
 
+template <typename ValueT>
 class Table {
   private:
-    Item* _table = new Item[CAPACITY];
+    Item<ValueT> *_table = new Item<ValueT>[CAPACITY];
     int _count;
 
     int findIndex(string key) {
@@ -34,14 +33,13 @@ class Table {
       if (finded) {
         return index;
       } else {
-        throw invalid_argument('element by this key does not exist');
+        throw invalid_argument("findIndex: element by this key does not exist");
       }
     }
 
   public:
     // default constructor
     Table() {
-      _table = nullptr;
       _count = 0;
     }
 
@@ -50,23 +48,26 @@ class Table {
   }
 
   bool contain(string key) {
-    try {
-      int index = findIndex(key);
-      return true;
-    } catch(const invalid_argument& err) {
-      cerr << "Invalid argument: " << err.what() << '\n';
-      return false;
+    bool finded = false;
+
+    for (int i = 0; i < CAPACITY; i++) {
+      if (_table[i].key == key) {
+        finded = true;
+        break;
+      }
     }
+
+    return finded;
   }
 
-  void put(string key, string new_value) {
+  void put(string key, ValueT new_value) {
     if (contain(key)) {
       int index = findIndex(key);
 
       _table[index].value = new_value;
     } else if (_count < CAPACITY) {
       for (int i = 0; i < CAPACITY; i++) {
-        if (_table[i].key == null) {
+        if (_table[i].key == "") {
           _table[i].key = key;
           _table[i].value = new_value;
           _count++;
@@ -74,7 +75,7 @@ class Table {
         }
       }
     } else {
-      throw length_error('table is full');
+      throw length_error("put: table is full");
     }
   }
 
@@ -82,41 +83,90 @@ class Table {
     if (contain(key)) {
       int index = findIndex(key);
 
-      _table[index].key = null;
-      _table[index].value = null;
+      _table[index].key = "";
+      _table[index].value = nullptr;
       _count--;
     } else {
-      throw invalid_argument('element by this key does not exist');
+      throw invalid_argument("erase: element by this key does not exist");
     }
   }
 
-  string valueAt(string key) {
+  ValueT valueAt(string key) {
     if (contain(key)) {
       int index = findIndex(key);
 
       return _table[index].value;
     } else {
-      throw invalid_argument('element by this key does not exist');
+      throw invalid_argument("valueAt: element by this key does not exist");
     }
   }
 
-  // copy constructor Table(const Table&)
-  // Table(const Table& copy) {
-    // 0. Initialize fields
+  void keysDo(void (*KeyDelegate)(string)) {
+    for (int i = 0; i < CAPACITY; i++) {
+      if (_table[i].key != "") {
+        (*KeyDelegate)(_table[i].key);
+      }
+    }
+  }
 
-  // }
+  void valuesDo(void (*ValueDelegate)(ValueT)) {
+    for (int i = 0; i < CAPACITY; i++) {
+      if (_table[i].key != "") {
+        (*ValueDelegate)(_table[i].value);
+      }
+    }
+  }
+
+  string* getKeys() {
+    string keys[CAPACITY];
+
+    for (int i = 0; i < CAPACITY; i++) {
+      keys[i] = _table[i].key;
+    }
+
+    return keys;
+  }
+
+  ValueT* getValues() {
+    ValueT values[CAPACITY];
+
+    for (int i = 0; i < CAPACITY; i++) {
+      if (_table[i].key != "") {
+        values[i] = _table[i].value;
+      } else {
+        values[i] = NULL;
+      }
+    }
+
+    return values;
+  }
+
+  // copy constructor Table(const Table&)
+  Table(const Table& copy) {
+    _count = copy._count;
+
+    for (int i = 0; i < CAPACITY; i++) {
+      _table[i].key = copy._table[i].key;
+      _table[i].value = copy._table[i].value;
+    }
+  }
 
   // assignment operator function
-  // Table operator=(const Table &st) {
+  Table operator=(const Table &tb) {
     /* SELF ASSIGNMENT CHECK */
-    // if(this != &st) {
-      // 1. Deallocate old memory
-      
-    // }
+    if(this != &tb) {
+      _count = tb._count;
+
+      // 3. Copy data from tb
+      for (int i = 0; i < CAPACITY; i++) {
+        _table[i].key = tb._table[i].key;
+        _table[i].value = tb._table[i].value;
+      }
+    }
 
     // 4. Return current object
-    // return *this;
-  // }
+    return *this;
+  }
 
   ~Table() {
     delete[] _table;
